4a9c54d528b662e81f87f81a953fbb39ea84a146
117marta <117marta@gmail.com>
2022-01-11 16:20:28 +0100

    AddRoom view

--- project/project/urls.py
+++ project/project/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.views.generic import TemplateView
+from reservation_app.views import AddRoomView
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('test/', TemplateView.as_view(template_name='reservation_app/base.html')),
+    path('room/view/', AddRoomView.as_view(), name='add-room')
 ]
--- project/reservation_app/views.py
+++ project/reservation_app/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views import View
+from .models import Room
 
-# Create your views here.
+
+class AddRoomView(View):
+
+    def get(self, request):
+        return render(request, 'reservation_app/add_room.html')
+
+    def post(self, request):
+        name = request.POST.get('room-name')
+        capacity = request.POST.get('capacity')
+        capacity = int(capacity) if capacity else 0
+        projector = request.POST.get('projector')  == 'on'  # rzutowanie na typ logiczny, sprawdzając czy wartość (on) jest ustawiona
+
+        # Błędy
+        if not name:  # Czy nazwa nie jest pusta
+            return render(request, 'reservation_app/add_room.html', context={'error': 'Nie podano nazwy sali!'})
+        if capacity <= 0:  # Czy pojemność sali jest dodatnia
+            return render(request, 'reservation_app/add_room.html', context={'error': 'Pojemność musi być dodatnia!'})
+        if Room.objects.filter(name=name).first():
+            return render(request, 'reservation_app/add_room.html', context={'error': 'Podana nazwa sali już istnieje!'})
+
+        # Dodanie nowego obiektu
+        Room.objects.create(name=name, capacity=capacity, projector_availability=projector)
+        return redirect('room-list')
